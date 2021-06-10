2fbf46adaa15dc64c259e6e30a0690c0ad5e05e9
Zaidali45 <85540324+Zaidali45@users.noreply.github.com>
2021-06-10 04:44:48 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+// alert("Hello word");
+
+// var a = 10;
+// var b =20;
+// var c = a+b;
+// alert(a)
+
+// var a = 10;
+// var b = 20;
+// var c =a+b;
+// console.log(c);
+
+// var y = 5;
+// var x = "ali";
+// var z = x+y;
+// alert(z);
+
+
+
+// var f = 20;
+// var g = "2";
+// var z = f-g;
+// alert(z);
+
+// var a =3;
+// var b =5;
+// var c =7;
+// var d =b - a * c;
+// alert(d);
+
+// var a =5;
+// var b =a++;
+// alert(b);
+
+var x =20;
+var y =x++ + ++x - x + x++ - ++x + x;
+        
+alert(y);
